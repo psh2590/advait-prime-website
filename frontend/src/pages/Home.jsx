ec4e80d41646b6024f675ecd62720236e60f3d3e
psh2590 <piyush.ajpandey@gmail.com>
2026-01-31 00:40:51 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,18 @@
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import Healthcare from "../components/Healthcare";
+import About from "../components/About";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Healthcare />
+      <About />
+      <Contact />
+    </>
+  );
+}
 
