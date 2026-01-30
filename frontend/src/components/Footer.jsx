a7fc2fa453911b89aa93795dd7b5e4d8aee659ab
psh2590 <piyush.ajpandey@gmail.com>
2026-01-30 23:41:03 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="bg-black text-white text-center py-4 text-sm">
-      © {new Date().getFullYear()} Advait Prime Consultancy & Services
+                  © 2025 Advait Prime Consultancy & Services. All rights reserved.
+      Disclaimer: Information on this website is provided for general informational purposes only.
     </footer>
   );
 }
+
