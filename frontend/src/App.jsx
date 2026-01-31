e822788ca30f58cefbb1faf282c6eca4b8b2812f
psh2590 <piyush.ajpandey@gmail.com>
2026-01-31 10:43:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
-import Healthcare from "./pages/Healthcare";
+import About from "./pages/About";
 import RealEstate from "./pages/RealEstate";
 import Business from "./pages/Business";
-import Leadership from "./pages/Leadership";
+import Healthcare from "./pages/Healthcare";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services/healthcare" element={<Healthcare />} />
-        <Route path="/services/real-estate" element={<RealEstate />} />
-        <Route path="/services/business" element={<Business />} />
-        <Route path="/services/leadership" element={<Leadership />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Advisory Pages */}
+        <Route path="/real-estate-advisory" element={<RealEstate />} />
+        <Route path="/business-advisory" element={<Business />} />
+        <Route path="/healthcare-advisory" element={<Healthcare />} />
+
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
     </BrowserRouter>
   );
