9d9ba162fa328fac4f057bc1f3a838709a4f5eb0
psh2590 <piyush.ajpandey@gmail.com>
2026-01-31 11:54:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,6 +14,7 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <BrowserRouter>
+     <ScrollToTop />
       <Navbar />
 
       <Routes>
